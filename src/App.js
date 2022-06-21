e99d49142305fbc0458f39f66f68ceb95817d360
pelumi454 <amoochristianah454@gmail.com>
2022-06-21 12:29:21 +0100

    fix: create component folder and context folder in src folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContextProvider";
+import LoadingSpinner from "./components/LoadingSpinner";
+import Home from "./components/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <h5>Hello world</h5>
-    </div>
-  );
+  const authContext = useContext(AuthContext);
+  if (!authContext.isAuthenticated) {
+    return <LoadingSpinner />;
+  } else {
+    return <Home />;
+  }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import heroBg from "../../src/assets/images/hero-phone-bg.svg";
+import './home.css'
+
+const Home = () => {
+
+  return (
+    <section>
+          <div className="slider-section">
+            <div className="slider-wrap">
+              <div className="slider-text-wrap">
+                <h1 className="username">Hello </h1>
+                <div className="slider-text">Shop the Latest Iphones</div>
+                <button className="show-now">Shop Now</button>
+                <button
+                  className="text-white bg-red-400 border-0 py-2 px-8 text-lg mt-10 logout-btn show-now"
+                  // onClick={authContext.logout}
+                >
+                  Logout
+                </button>
+              </div>
+              <div className="slider-image-wrap">
+                <img src={heroBg} alt="" className="slider-image" />
+              </div>
+            </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
