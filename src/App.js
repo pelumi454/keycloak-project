a67a60a723145b7c180867f509b0e97f3110fd36
pelumi454 <amoochristianah454@gmail.com>
2022-06-21 15:09:14 +0100

    fix: clean up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import { useContext } from "react";
-import { AuthContext } from "./context/AuthContextProvider";
-import LoadingSpinner from "./components/LoadingSpinner";
+
 import Home from "./components/Home";
 
 function App() {
   return <Home />;
-  // if (!authContext.isAuthenticated) {
-  //   return <LoadingSpinner />;
-  // } else {
-  //   return <Home />;
-  // }
 }
 
 export default App;
