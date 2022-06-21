2442ea9134ea8902fbc5ad57a8396dce33ed996b
pelumi454 <amoochristianah454@gmail.com>
2022-06-21 15:02:03 +0100

    fix: create keycloak instance

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,36 @@
 import React from "react";
 import heroBg from "../../src/assets/images/hero-phone-bg.svg";
 import './home.css'
+import { login, logout } from "../context/AuthContextProvider";
+import { kck } from "../context/AuthContextProvider";
 
 const Home = () => {
-
+console.log(kck)
   return (
     <section>
-          <div className="slider-section">
-            <div className="slider-wrap">
-              <div className="slider-text-wrap">
-                <h1 className="username">Hello </h1>
-                <div className="slider-text">Shop the Latest Iphones</div>
-                <button className="show-now">Shop Now</button>
-                <button
-                  className="text-white bg-red-400 border-0 py-2 px-8 text-lg mt-10 logout-btn show-now"
-                  // onClick={authContext.logout}
-                >
-                  Logout
-                </button>
-              </div>
-              <div className="slider-image-wrap">
-                <img src={heroBg} alt="" className="slider-image" />
-              </div>
-            </div>
+      <div className="slider-section">
+        <div className="slider-wrap">
+          <div className="slider-text-wrap">
+            <h1 className="username">Hello </h1>
+            <div className="slider-text">Shop the Latest Iphones</div>
+            <button className="show-now">Shop Now</button>
+            <button
+              className="text-white bg-red-400 border-0 py-2 px-8 text-lg mt-10 logout-btn show-now"
+              onClick={() => login()}
+            >
+              Login
+            </button>
+            <button
+              className="text-white bg-red-400 border-0 py-2 px-8 text-lg mt-10 logout-btn show-now"
+              onClick={() => logout()}
+            >
+              Logout
+            </button>
+          </div>
+          <div className="slider-image-wrap">
+            <img src={heroBg} alt="" className="slider-image" />
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/context/AuthContextProvider.js
+++ src/context/AuthContextProvider.js
@@ -1,1 +1,30 @@
-import 
+import Keycloak from "keycloak-js";
+import axios from "axios";
+export const kck = new Keycloak("/keycloak.json");
+export const initKeycloak = (myApp) => {
+  kck.init({
+    onLoad: "check-sso",
+    silentCheckSsoRedirectUri: "http://localhost:3000/silent-check-sso",
+    pkceMethod: "S256",
+  });
+};
+const myAxios = axios.create();
+export const login = kck.login;
+export const logout = kck.logout;
+export const isLoggedIn = () => {
+  return !!kck.token;
+};
+const updateToken = (successCallback) => { 
+  kck.updateToken(5).then(successCallback)
+}
+export const configureApp = () => {
+  myAxios.interceptors.request.use((config) => {
+    if (isLoggedIn()) {
+      const callback = () => {
+        config.headers.Authorization = `Bearer ${kck.token}`;
+        return Promise.resolve(config);
+      };
+      return updateToken(callback);
+    }
+  });
+};
